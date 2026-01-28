58767fb4376563e993869ec0e7cd3895cb5f5151
Hamid-GenAI-Eng <hamidsaif214@gmail.com>
2026-01-28 17:09:09 +0500

    Invoice error

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const { loginUser, createUser } = require('../controllers/authController');
+// 1. Import getAllUsers and deleteUser from controller
+const { 
+  loginUser, 
+  createUser, 
+  getAllUsers, 
+  deleteUser 
+} = require('../controllers/authController');
+
 const { protect, admin } = require('../middlewares/authMiddleware');
 
 // Public Route
 router.post('/login', loginUser);
 
-// Protected Route (Only Super Admin can create new users)
+// Protected Routes
 router.post('/create-user', protect, admin, createUser);
+
+// 2. Add this route (Fixes the "Disappear on Refresh" issue)
+router.get('/users', protect, admin, getAllUsers);
+
+// 3. Add this route (Allows deleting users)
+router.delete('/users/:id', protect, admin, deleteUser);
 
 module.exports = router;
